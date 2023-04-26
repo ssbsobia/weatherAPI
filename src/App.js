d1f5f8acb8d1ee80f0c1614e43aad060cc29dfbc
Sobia Mumtaz <ssb.sobia@gmail.com>
2023-04-26 16:10:35 +0500

    weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchButton = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&APPID=046969aae2d05b6bebfb15233fb921f2`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result)
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container grid">
+      
+      <div className="weather">
+        {/* Search Box */}
+    
+<select placeholder='Select City' onChange={(e) => setCity(e.target.value)}>
+  <option value="" disabled selected>Select</option>
+  <option>Lahore</option>
+  <option>Islamabad</option>
+  <option>BhahawalNagar</option>
+  <option>Multan</option>
+
+</select>
+        {/* <input type="text" placeholder="Country" onChange={(e) => setCountry(e.target.value)} />
+        <input type="text" placeholder="City" onChange={(e) => setCity(e.target.value)} /> */}
+        <button onClick={searchButton} className='contrast'>Search</button>
+      </div>
+      {typeof weather.main !== "undefined" ? (
+        <div>
+          <p>{weather.name}</p>
+          <p>{weather.main.temp}°C</p>
+          <p>{weather.weather[0].main}</p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
